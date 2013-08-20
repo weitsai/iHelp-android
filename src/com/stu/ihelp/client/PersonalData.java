8035323ad25ac5be8e9860e96b976587ea6b60f2
weitsai <begining1003@gmail.com>
2013-08-20 18:02:33 +0800

    移除個人資料 Dialog title

--- src/com/stu/ihelp/client/PersonalData.java
+++ src/com/stu/ihelp/client/PersonalData.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,6 +21,7 @@ public class PersonalData extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.personal_data);
 
 		et_name = (EditText) findViewById(R.id.edit_name);
