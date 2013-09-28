164273d3c45b38dec774372cbf36e5468fd9dc6d
weitsai <begining1003@gmail.com>
2013-09-28 11:15:24 +0800

    若是 sim card 有問題則無法使用報案功能

--- app/src/edu/stu/ihelp/client/IHelpActivity.java
+++ app/src/edu/stu/ihelp/client/IHelpActivity.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Toast;
 
@@ -43,8 +45,29 @@ public class IHelpActivity extends Activity {
     }
 
     public void onClickGeneral(View v) {
-        startActivityForResult(new Intent(IHelpActivity.this, General.class),
-                INTENT_GENERAL);
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        switch (tm.getSimState()) {
+        case TelephonyManager.SIM_STATE_ABSENT:
+            Toast.makeText(this, "若沒有插入 sim 卡可能無法使用該服務", 0).show();
+            break;
+        case TelephonyManager.SIM_STATE_UNKNOWN:
+            Toast.makeText(this, "sim 卡發生了不知名狀況請聯絡電信商", 0).show();
+            break;
+        case TelephonyManager.SIM_STATE_NETWORK_LOCKED:
+            Toast.makeText(this, "請先將 NetworkPIN 碼解鎖", 0).show();
+            break;
+        case TelephonyManager.SIM_STATE_PIN_REQUIRED:
+            Toast.makeText(this, "請先將 sim 卡 PIN 碼解鎖", 0).show();
+            break;
+        case TelephonyManager.SIM_STATE_PUK_REQUIRED:
+            Toast.makeText(this, "請先將 sim 卡 PUK 碼解鎖", 0).show();
+            break;
+        case TelephonyManager.SIM_STATE_READY:
+            startActivityForResult(
+                    new Intent(IHelpActivity.this, General.class),
+                    INTENT_GENERAL);
+            break;
+        }
     }
 
     @Override
